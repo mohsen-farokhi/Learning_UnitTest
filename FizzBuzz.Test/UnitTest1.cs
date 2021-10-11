7b9cc25019fc3e68fd8e437043b9d207bda07c12
Mohsen Farokhi <m.farokhi@outlook.com>
2021-10-11 16:04:08 +0330

    init

--- FizzBuzz.Test/UnitTest1.cs
+++ FizzBuzz.Test/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Xunit;
 
 namespace FizzBuzz.Test
@@ -5,9 +6,17 @@ namespace FizzBuzz.Test
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Returns_numbers_when_not_divided_by_3_or_5()
         {
+            var result = FizzBuzzGenerator.Generate(1, 2);
+        }
+    }
 
+    public class FizzBuzzGenerator
+    {
+        public static IList<string> Generate(int start, int end)
+        {
+            return null;
         }
     }
 }
